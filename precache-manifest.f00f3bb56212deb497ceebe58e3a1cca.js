bd706f2a40812661f9a3c909824d6a7a42b95da3
Mikael Pettersson <evanion@icloud.com>
2019-10-02 17:53:06 +0200

    Updates

---
+++ precache-manifest.f00f3bb56212deb497ceebe58e3a1cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aea1989878d922c4fc80f893ad2e115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0214ec2eedecd7cbc30c",
+    "url": "/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "87a7faabbcec74f0a898",
+    "url": "/static/js/2.eecda222.chunk.js"
+  },
+  {
+    "revision": "0214ec2eedecd7cbc30c",
+    "url": "/static/js/main.db245d66.chunk.js"
+  },
+  {
+    "revision": "f1697608289c8a40ab07",
+    "url": "/static/js/runtime-main.6b7a256f.js"
+  }
+]);
